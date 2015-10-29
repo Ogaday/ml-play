9a6fc84c65711cfcb5080a1fcb7461ea7a505887
Ogaday <W.Ogaday@gmail.com>
2015-10-29 20:20:46 +0000

    Successfully subclass test classes

--- ann/tests/test_graph.py
+++ ann/tests/test_graph.py
@@ -1,8 +1,29 @@
 import unittest
-#from ann.graph import Graph
 from ann.graph import Graph
 
 print(__name__)
-class TestClass (unittest.TestCase):
+class TestClass(unittest.TestCase):
     def test(self):
         pass
+
+class TestGraphBase(unittest.TestCase):
+    def setUp(self):
+        self.arg=5
+        self.out=5
+
+    def testExpectedOutputs(self):
+        "This test should pass"
+        self.assertEqual(self.arg, self.out)
+
+class TestGraphSimple(TestGraphBase):
+    """
+      o----o
+     / \  / \
+    o   \/   o
+     \  /\  /
+      \/  \/
+       o---o
+    """
+    def setUp(self):
+        self.arg=6
+        self.out=6
