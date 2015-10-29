464705c59395c49ce6c40e42e8d859f86f75bbb4
Ogaday <W.Ogaday@gmail.com>
2015-10-29 19:44:06 +0000

    Move test files to within package directory

---
+++ ann/tests/README.md
@@ -0,0 +1,9 @@
+Tests
+=====
+
+Nosetest module for my ml modules.
+
+To run tests:
+```
+(ml-env)user@computer:~/.../ml-play nosetests
+```
---
+++ ann/tests/test_graph.py
@@ -0,0 +1,8 @@
+import unittest
+#from ann.graph import Graph
+from ann.graph import Graph
+
+print(__name__)
+class TestClass (unittest.TestCase):
+    def test(self):
+        pass
