896c1c65f312312b709b6b2a44f373ee4037b6d0
Ogaday <W.Ogaday@gmail.com>
2015-10-30 19:19:08 +0000

    Update tests for new ANN class

--- ann/tests/test_ann.py
+++ ann/tests/test_ann.py
@@ -1,28 +1,36 @@
 import unittest
-from ann.graph import Graph
+from ann.ann import ANN
 
-class TestGraphBase(unittest.TestCase):
+class TestANNBase(unittest.TestCase):
     def setUp(self):
-        self.G=Graph([[0]])
+        self.net=ANN([[0]], sum)
         self.costs={(0,0):0}    # pair: cost dict.
         self.heads={0:()}
         self.tails={0:()}
+        self.sources=(0,)
+        self.sinks=(0,)
 
     def testCosts(self):
         # print(self.costs)
         for pair, c in self.costs.items():
             # print(pair, c)
-            self.assertEqual(self.G.cost(pair[0], pair[1]), c)
+            self.assertEqual(self.net.cost(pair[0], pair[1]), c)
 
     def testHeads(self):
         for V, heads in self.heads.items():
-            self.assertEqual(tuple(self.G.heads(V)), heads)
+            self.assertEqual(tuple(self.net.heads(V)), heads)
 
     def testTails(self):
         for V, tails in self.tails.items():
-            self.assertEqual(tuple(self.G.tails(V)), tails)
+            self.assertEqual(tuple(self.net.tails(V)), tails)
 
-class TestGraphSimple(TestGraphBase):
+    def testSources(self):
+        self.assertEqual(tuple(self.net.sources()), self.sources)
+
+    def testSinks(self):
+        self.assertEqual(tuple(self.net.sinks()), self.sinks)
+
+class TestNetSimple(TestANNBase):
     """
     --->
          1   3
@@ -35,15 +43,16 @@ class TestGraphSimple(TestGraphBase):
          2    4
     """
     def setUp(self):
-        self.G=Graph(matrix = 
-                     [[0,1,1,-1,-1,-1],
+        self.net=ANN([[0,1,1,-1,-1,-1],
                       [-1,0,-1,1,1,-1],
                       [-1,-1,0,1,1,-1],
                       [-1,-1,-1,0,-1,1],
                       [-1,-1,-1,-1,0,1],
-                      [-1,-1,-1,-1,-1,0]])
+                      [-1,-1,-1,-1,-1,0]], sum)
         self.costs={(i,i):0 for i in range(6)}
         rest={(0,1):1,(0,2):1,(1,3):1,(1,4):1,(2,3):1,(2,4):1,(3,5):1,(4,5):1}
         self.costs.update(rest)
         self.heads={0:(), 1:(0,), 2:(0,), 3:(1,2), 4:(1,2), 5:(3,4)}
         self.tails={0:(1,2), 1:(3,4), 2:(3,4), 3:(5,), 4:(5,), 5:()}
+        self.sources = (0,)
+        self.sinks = (5,)
