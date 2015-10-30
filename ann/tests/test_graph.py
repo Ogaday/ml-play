36f8ed7a6188df04dbeab970e99d5d73d33b23d0
Ogaday <W.Ogaday@gmail.com>
2015-10-30 16:22:09 +0000

    Write initial tests checking elemtentary methods

--- ann/tests/test_graph.py
+++ ann/tests/test_graph.py
@@ -1,29 +1,49 @@
 import unittest
 from ann.graph import Graph
 
-print(__name__)
-class TestClass(unittest.TestCase):
-    def test(self):
-        pass
-
 class TestGraphBase(unittest.TestCase):
     def setUp(self):
-        self.arg=5
-        self.out=5
+        self.G=Graph([[0]])
+        self.costs={(0,0):0}    # pair: cost dict.
+        self.heads={0:()}
+        self.tails={0:()}
 
-    def testExpectedOutputs(self):
-        "This test should pass"
-        self.assertEqual(self.arg, self.out)
+    def testCosts(self):
+        print(self.costs)
+        for pair, c in self.costs.items():
+            print(pair, c)
+            self.assertEqual(self.G.cost(pair[0], pair[1]), c)
+
+    def testHeads(self):
+        for V, heads in self.heads.items():
+            self.assertEqual(tuple(self.G.heads(V)), heads)
+
+    def testTails(self):
+        for V, tails in self.tails.items():
+            self.assertEqual(tuple(self.G.tails(V)), tails)
 
 class TestGraphSimple(TestGraphBase):
     """
-      o----o
-     / \  / \
-    o   \/   o
-     \  /\  /
-      \/  \/
-       o---o
+    --->
+         1   3
+         o----o
+        / \  / \
+     0 o   \/   o 5
+        \  /\  /
+         \/  \/
+         o----o
+         2    4
     """
     def setUp(self):
-        self.arg=6
-        self.out=6
+        self.G=Graph(matrix = 
+                     [[0,1,1,-1,-1,-1],
+                      [-1,0,-1,1,1,-1],
+                      [-1,-1,0,1,1,-1],
+                      [-1,-1,-1,0,-1,1],
+                      [-1,-1,-1,-1,0,1],
+                      [-1,-1,-1,-1,-1,0]])
+        self.costs={(i,i):0 for i in range(6)}
+        rest={(0,1):1,(0,2):1,(1,3):1,(1,4):1,(2,3):1,(2,4):1,(3,5):1,(4,5):1}
+        self.costs.update(rest)
+        self.heads={0:(), 1:(0,), 2:(0,), 3:(1,2), 4:(1,2), 5:(3,4)}
+        self.tails={0:(1,2), 1:(3,4), 2:(3,4), 3:(5,), 4:(5,), 5:()}
