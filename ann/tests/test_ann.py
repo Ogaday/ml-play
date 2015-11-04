2f3a8192282d08f6015f89a8aa3975b7b05906cb
Ogaday <W.Ogaday@gmail.com>
2015-11-04 15:31:36 +0000

    Add Perceptron tests, fix testing of feedforward

--- ann/tests/test_ann.py
+++ ann/tests/test_ann.py
@@ -1,6 +1,7 @@
 import unittest
 from ann.ann import ANN
 from ann.tests.test_graph import TestGraphBase
+from ann.functions import heaviside
 
 class TestANNBase(TestGraphBase):
     def setUp(self):
@@ -14,14 +15,18 @@ class TestANNBase(TestGraphBase):
         # Should I be returning tuples or lists?
         self.outputs={(0.1,):{0:0.1},(1,):{0:1},(5,):{0:5}}
 
-    def testFeedforward0_1(self):
-        self.assertEqual(self.G.feedforward((0.1,)), self.outputs[(0.1,)])
+    def testFeedforward(self):
+        for inputs, output in self.outputs.items():
+            self.assertEqual(self.G.feedforward(inputs), output)
 
-    def testFeedforward1(self):
-        self.assertEqual(self.G.feedforward((1,)),self.outputs[(1,)])
-
-    def testFeedforward5(self):
-        self.assertEqual(self.G.feedforward((5,)),self.outputs[(5,)])
+#    def testFeedforward0_1(self):
+#        self.assertEqual(self.G.feedforward((0.1,)), self.outputs[(0.1,)])
+#
+#    def testFeedforward1(self):
+#        self.assertEqual(self.G.feedforward((1,)),self.outputs[(1,)])
+#
+#    def testFeedforward5(self):
+#        self.assertEqual(self.G.feedforward((5,)),self.outputs[(5,)])
 
 class TestNetSimple(TestANNBase):
     """
@@ -51,3 +56,85 @@ class TestNetSimple(TestANNBase):
         self.sinks = (5,)
         self.order = 6
         self.outputs={(0.1,):{5:0.4},(1,):{5:4},(5,):{5:20}}
+
+class TestPerceptron(TestANNBase):
+    """
+    --->
+    2
+    o
+     \
+    3 \0  1
+    o--o--o
+      /
+    4/
+    o
+
+    """
+    def setUp(self):
+        self.G=ANN([[ 0, 1,-1,-1,-1],
+                   [-1, 0,-1,-1,-1],
+                   [0.3,-1, 0,-1,-1],
+                   [0.5,-1,-1,-1, 0],
+                   [ 1,-1,-1, 0,-1]], sum)
+        self.costs={(2,0):0.3, (3,0):0.5, (4,0):1, (0,1):1}
+        self.heads={2:(), 3:(), 4:(), 0:(2,3,4), 1:(0,)}
+        self.tails={2:(0,), 3:(0,), 4:(0,), 0:(1,), 1:()}
+        self.sources=(2,3,4)
+        self.sinks=(1,)
+        self.order=5
+        self.outputs={(0,0,0):{1:0}, (1,1,1):{1:1.8}, (-1,1,1):{1:1.2},
+                      (0.5,0.3,1):{1:1.3}}
+
+class TestPerceptronHeaviside(TestANNBase):
+    """
+    --->
+    2
+    o
+     \
+    4 \0  1
+    o--o--o
+      /
+    3/
+    o
+
+    """
+    def setUp(self):
+        self.G=ANN([[ 0, 1,-1,-1,-1],
+                   [-1,  0,-1,-1,-1],
+                   [0.3,-1, 0,-1,-1],
+                   [0.5,-1,-1,0, -1],
+                   [ 1,-1,-1, -1, 0]], heaviside)
+        self.costs={(2,0):0.3, (3,0):0.5, (4,0):1, (0,1):1}
+        self.heads={2:(), 3:(), 4:(), 0:(2,3,4), 1:(0,)}
+        self.tails={2:(0,), 3:(0,), 4:(0,), 0:(1,), 1:()}
+        self.sources=(2,3,4)
+        self.sinks=(1,)
+        self.order=5
+        self.outputs={}
+
+class TestGershenson1(TestANNBase):
+    """
+    --->
+    0  2     4  6
+    o--o-----o--o
+        \   /
+         \ /
+          x
+         / \
+        /   \
+    o--o-----o--o
+    1  3     5  7
+    """
+    def setUp(self):
+        self.G=ANN([[ 0, 1,-1,-1,-1],
+                   [-1, 0,-1,-1,-1],
+                   [0.3,-1, 0,-1,-1],
+                   [0.5,-1,-1,-1, 0],
+                   [ 1,-1,-1, 0,-1]], sum)
+        self.costs={(2,0):0.3, (3,0):0.5, (4,0):1, (0,1):1}
+        self.heads={2:(), 3:(), 4:(), 0:(2,3,4), 1:(0,)}
+        self.tails={2:(0,), 3:(0,), 4:(0,), 0:(1,), 1:()}
+        self.sources=(2,3,4)
+        self.sinks=(1,)
+        self.order=5
+        self.outputs={}
